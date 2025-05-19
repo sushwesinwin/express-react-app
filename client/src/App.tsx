a2e6534cdf938de6924c76c2771dacc26152e0f0
Su Shwe Sin Win <sushwesinw@gmail.com>
2025-05-20 06:29:18 +0630

    Initial commit: Express + React setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
+
+export default function App() {
+  return (
+    <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </>
+  )
+}
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white dark:bg-gray-900 fixed z-20 w-full top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      hi
+
+    </nav>
+  )
+}
